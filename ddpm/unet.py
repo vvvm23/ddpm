e58abe78653b139d6fad16ef2836f72c3fd94c63
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2021-09-27 11:38:00 +0100

    completes (untested) UNet architecture

--- ddpm/unet.py
+++ ddpm/unet.py
@@ -30,18 +30,20 @@ class AttentionBlock(HelperModule):
     def __init__(self, dim, heads = 4, dim_head = 32):
         self.heads = heads
         hidden_dim = dim_head * heads
+        self.pre_norm = nn.LayerNorm(dim)
         self.to_qkv = nn.Conv2d(dim, hidden_dim * 3, 1, bias = False)
         self.to_out = nn.Conv2d(hidden_dim, dim, 1)
 
     def forward(self, x):
         b, c, h, w = x.shape
+        x = self.pre_norm(x)
         qkv = self.to_qkv(x)
         q, k, v = rearrange(qkv, 'b (qkv heads c) h w -> qkv b heads c (h w)', heads = self.heads, qkv=3)
         k = k.softmax(dim=-1)
         context = torch.einsum('bhdn,bhen->bhde', k, v)
         out = torch.einsum('bhde,bhdn->bhen', context, q)
         out = rearrange(out, 'b heads c (h w) -> b (heads c) h w', heads=self.heads, h=h, w=w)
-        return self.to_out(out)
+        return self.to_out(out) + x
 
 class Block(HelperModule):
     def build(self,
@@ -87,13 +89,13 @@ class ResnetBlock(HelperModule):
         if scale_norm:
             self.block_out = ScaleBlock(dim_out, dim_out, groups=groups)
             self.emb = nn.Sequential(
-                nn.Linear(dim_out, 2*dim_out),
+                nn.Linear(dim_emb, 2*dim_out),
                 nn.SiLU()
             )
         else:
             self.block_out = Block(dim_out, dim_out, groups=groups)
             self.emb = nn.Sequential(
-                nn.Linear(dim_out, dim_out),
+                nn.Linear(dim_emb, dim_out),
                 nn.SiLU()
             )
         self.res_block = nn.Conv(dim_in, dim_out, 1) if dim_in != dim_out else nn.Identity()
@@ -104,22 +106,57 @@ class ResnetBlock(HelperModule):
         h = self.block_out(h, t_emb)
         return self.res_block(x) + h
 
+class UNetSampleType:
+    DOWN = 'down'
+    UP = 'up'
+    SAME = 'same'
+
+class SampleLayer(HelperModule):
+    def build(self,
+        dim: int,
+        sample_type: UNetSampleType,
+    ):
+        if sample_type == UNetSampleType.DOWN:
+            self.layer = nn.Conv2d(dim, dim, 3, 2, 1)
+        elif sample_type == UNetSampleType.UP:
+            self.layer = nn.ConvTranspose2d(dim, dim, 4, 2, 1)
+        elif sample_type == UNetSampleType.SAME:
+            self.layer = nn.Identity()
+        else:
+            raise ValueError(f"invalid sampling strategy '{sample_type}'")
+
+    def forward(self, x):
+        return self.layer(x)
+
 class UNetLayer(HelperModule):
     def build(self,
             dim_in: int,
             dim_out: int,
             dim_emb: int,
-            downsample: bool = True, #2x downsampling
+            sample_type: UNetSampleType,
+            nb_res_layers: int = 3,
             attention: bool = False,
             nb_heads: int = 4,
             dim_head: int = 32,
             scale_norm: bool = False,
             groups: int = 8,
         ):
-        pass
+        res_dims = [dim_in] + [dim_out] * (nb_res_layers - 1)
+        
+        self.blocks = nn.ModuleList([
+            ResnetBlock(di, do, dim_emb, scale_norm=scale_norm, groups=groups)
+            for di, do in zip(res_dims[:-1], res_dims[1:])
+        ])
 
-    def forward(self, x):
-        pass
+        self.attn = AttentionBlock(dim_out, nb_heads, dim_head) if attention else nn.Identity()
+        self.sample = SampleLayer(dim_out, sample_type)
+
+    def forward(self, x, t):
+        for res in self.blocks:
+            x = res(x, t)
+        h = self.attn(x)
+        x = self.sample(h)
+        return x, h
 
 class UNet(HelperModule):
     # TODO: add dropout
@@ -127,13 +164,91 @@ class UNet(HelperModule):
             in_channels: int = 3,
             base_channels: int = 128,
             channel_multipliers: List[int] = [1, 2, 2, 2],
+            dim_t: int = 64, # TODO: sane?
             attention_resolutions: List[int] = [2, 4],
-            nb_res_blocks: int = 3,
+            nb_res_layers: int = 3,
             nb_heads: int = 4,
             dim_head: int = 32,
+            scale_norm: bool = False,
             norm_groups: int = 8,
         ):
-        pass
 
-    def forward(self, x):
-        pass
+        self.dim_t = dim_t
+        self.time_mlp = nn.Sequential(
+            nn.Linear(dim_t, dim_t*4),
+            nn.SiLU(),
+            nn.Linear(dim_t*4, dim_t),
+        )
+
+        cumulative_sample = 1
+        channels = [in_channels] + [base_channels * cm for cm in channel_multipliers]
+        last_idx = len(channels) - 2
+
+        self.down_layers = nn.ModuleList()
+        for i, (ci, co) in enumerate(zip(channels[:-1], channels[1:])):
+            self.down_layers.append(
+                UNetLayer(
+                    dim_in = ci, 
+                    dim_out = co,
+                    dim_emb = dim_t,
+                    sample_type = UNetSampleType.SAME if i == last_idx else UNetSampleType.DOWN,
+                    nb_res_layers = nb_res_layers,
+                    attention = cumulative_sample in attention_resolutions,
+                    nb_heads = nb_heads,
+                    dim_head = dim_head,
+                    scale_norm = scale_norm,
+                    groups = norm_groups,
+                )
+            )
+            cumulative_sample *= 1 if i == last_idx else 2
+
+        self.mid_block = UNetLayer(
+            dim_in = channels[-1], 
+            dim_out = channels[-1],
+            dim_emb = dim_t,
+            sample_type = UNetSampleType.SAME,
+            nb_res_layers = nb_res_layers,
+            attention = cumulative_sample in attention_resolutions,
+            nb_heads = nb_heads,
+            dim_head = dim_head,
+            scale_norm = scale_norm,
+            groups = norm_groups,
+        )
+
+        self.up_layers = nn.ModuleList()
+        for i, (ci, co) in enumerate(zip(channels[:0:-1], channels[-2::-1])):
+            self.down_layers.append(
+                UNetLayer(
+                    dim_in = ci*2, 
+                    dim_out = co,
+                    dim_emb = dim_t,
+                    sample_type = UNetSampleType.SAME if i == last_idx else UNetSampleType.UP,
+                    nb_res_layers = nb_res_layers,
+                    attention = cumulative_sample in attention_resolutions,
+                    nb_heads = nb_heads,
+                    dim_head = dim_head,
+                    scale_norm = scale_norm,
+                    groups = norm_groups,
+                )
+            )
+            cumulative_sample //= 1 if i == last_idx else 2
+
+        self.out_block = nn.Sequential(
+            Block(in_channels, in_channels, groups=groups),
+            nn.Conv2d(in_channels, in_channels, 1),
+        )
+
+    def forward(self, x, t):
+        t_emb = self.time_mlp(timestep_embedding(t, self.dim_t))
+
+        activations = []
+        for l in self.down_layers:
+            x, h = l(x, t)
+            activations.append(h)
+
+        x, _ = self.mid_block(x, t)
+
+        for l in self.up_layers:
+            x, _ = l(torch.cat([x, h.pop()], dim=1))
+        
+        return self.out_block(x)
