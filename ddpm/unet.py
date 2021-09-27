5c3f5764cedf1cbf9a21c86eb58faca1be9d53b0
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2021-09-27 12:23:30 +0100

    Fixes UNet architecture Adds `LayerNorm` for multi-dimension from lucidrains repo to attn prenorm Makes emb in `Block` optional Fixes appending up layers do `down_layers` Fixes passing `t` rather than its mlp embedding Fixes popping from `t` rather than from `activations` Adds testing function, seems to be super memory hungry!

--- ddpm/unet.py
+++ ddpm/unet.py
@@ -11,7 +11,7 @@ from .utils import HelperModule
 
 # taken from:
 # https://github.com/openai/improved-diffusion/blob/e94489283bb876ac1477d5dd7709bbbd2d9902ce/improved_diffusion/nn.py
-# timesteps should be integers!
+# timesteps should be integers!!!!
 def timestep_embedding(timesteps, dim, max_period=10000):
     half = dim // 2
     freqs = torch.exp(
@@ -26,11 +26,25 @@ def timestep_embedding(timesteps, dim, max_period=10000):
 
 # taken from:
 # https://github.com/lucidrains/denoising-diffusion-pytorch/blob/master/denoising_diffusion_pytorch/denoising_diffusion_pytorch.py
+class LayerNorm(nn.Module):
+    def __init__(self, dim, eps = 1e-5):
+        super().__init__()
+        self.eps = eps
+        self.g = nn.Parameter(torch.ones(1, dim, 1, 1))
+        self.b = nn.Parameter(torch.zeros(1, dim, 1, 1))
+
+    def forward(self, x):
+        std = torch.var(x, dim = 1, unbiased = False, keepdim = True).sqrt()
+        mean = torch.mean(x, dim = 1, keepdim = True)
+        return (x - mean) / (std + self.eps) * self.g + self.b
+
+# taken from:
+# https://github.com/lucidrains/denoising-diffusion-pytorch/blob/master/denoising_diffusion_pytorch/denoising_diffusion_pytorch.py
 class AttentionBlock(HelperModule):
-    def __init__(self, dim, heads = 4, dim_head = 32):
+    def build(self, dim, heads = 4, dim_head = 32):
         self.heads = heads
         hidden_dim = dim_head * heads
-        self.pre_norm = nn.LayerNorm(dim)
+        self.pre_norm = LayerNorm(dim)
         self.to_qkv = nn.Conv2d(dim, hidden_dim * 3, 1, bias = False)
         self.to_out = nn.Conv2d(hidden_dim, dim, 1)
 
@@ -51,14 +65,16 @@ class Block(HelperModule):
             dim_out: int,
             groups: int = 8,
         ):
+        self.dim_in = dim_in
+        self.dim_out = dim_out
         self.layers = nn.Sequential(
             nn.Conv2d(dim_in, dim_out, 3, padding=1),
             nn.GroupNorm(groups, dim_out),
             nn.SiLU(),
         )
 
-    def forward(self, x, emb):
-        return self.layers(x + emb)
+    def forward(self, x, emb=None):
+        return self.layers(x + (0.0 if emb == None else emb))
 
 class ScaleBlock(HelperModule):
     def build(self,
@@ -98,7 +114,7 @@ class ResnetBlock(HelperModule):
                 nn.Linear(dim_emb, dim_out),
                 nn.SiLU()
             )
-        self.res_block = nn.Conv(dim_in, dim_out, 1) if dim_in != dim_out else nn.Identity()
+        self.res_block = nn.Conv2d(dim_in, dim_out, 1) if dim_in != dim_out else nn.Identity()
 
     def forward(self, x, t_emb):
         h = self.block_in(x)
@@ -116,6 +132,7 @@ class SampleLayer(HelperModule):
         dim: int,
         sample_type: UNetSampleType,
     ):
+        self.sample_type = sample_type
         if sample_type == UNetSampleType.DOWN:
             self.layer = nn.Conv2d(dim, dim, 3, 2, 1)
         elif sample_type == UNetSampleType.UP:
@@ -148,6 +165,7 @@ class UNetLayer(HelperModule):
             for di, do in zip(res_dims[:-1], res_dims[1:])
         ])
 
+        # TODO: add pre_norm (LayerNorm) before attention block
         self.attn = AttentionBlock(dim_out, nb_heads, dim_head) if attention else nn.Identity()
         self.sample = SampleLayer(dim_out, sample_type)
 
@@ -216,8 +234,9 @@ class UNet(HelperModule):
         )
 
         self.up_layers = nn.ModuleList()
+        channels = channels[1:]
         for i, (ci, co) in enumerate(zip(channels[:0:-1], channels[-2::-1])):
-            self.down_layers.append(
+            self.up_layers.append(
                 UNetLayer(
                     dim_in = ci*2, 
                     dim_out = co,
@@ -234,8 +253,8 @@ class UNet(HelperModule):
             cumulative_sample //= 1 if i == last_idx else 2
 
         self.out_block = nn.Sequential(
-            Block(in_channels, in_channels, groups=groups),
-            nn.Conv2d(in_channels, in_channels, 1),
+            # Block(in_channels, in_channels, groups=norm_groups),
+            nn.Conv2d(channels[0], in_channels, 1),
         )
 
     def forward(self, x, t):
@@ -243,12 +262,23 @@ class UNet(HelperModule):
 
         activations = []
         for l in self.down_layers:
-            x, h = l(x, t)
+            x, h = l(x, t_emb)
             activations.append(h)
 
-        x, _ = self.mid_block(x, t)
+        x, _ = self.mid_block(x, t_emb)
 
         for l in self.up_layers:
-            x, _ = l(torch.cat([x, h.pop()], dim=1))
+            x, _ = l(torch.cat([x, activations.pop()], dim=1), t_emb)
         
         return self.out_block(x)
+
+if __name__ == '__main__':
+    from .utils import get_parameter_count
+    N = 16
+    device = torch.device('cuda')
+    model = UNet(scale_norm=False).to(device)
+    print(f"> number of parameters: {get_parameter_count(model)}")
+    x = torch.randn(N, 3, 128, 128).to(device)
+    t = torch.randint(0, 5000, (N,)).to(device)
+    y = model(x, t)
+    print(y.shape)
--- ddpm/utils.py
+++ ddpm/utils.py
@@ -7,3 +7,5 @@ class HelperModule(torch.nn.Module):
 
     def build(self, *args, **kwargs):
         raise NotImplementedError
+def get_parameter_count(net: torch.nn.Module) -> int:
+    return sum(p.numel() for p in net.parameters() if p.requires_grad)
